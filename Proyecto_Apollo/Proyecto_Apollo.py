027eea235d1fc3a2ca4c1f947a292fe3dd313e69
bradrezdev <bradrez@Mac-mini-de-Bryan-Nunez.local>
2025-05-13 23:38:15 -0600

    Se agregó el API KEY del asistente a State

--- Proyecto_Apollo/Proyecto_Apollo.py
+++ Proyecto_Apollo/Proyecto_Apollo.py
@@ -1,44 +1,58 @@
+# chatapp.py
 import reflex as rx
+
 from Proyecto_Apollo import style
+from Proyecto_Apollo.state import State
+
 
 def qa(question: str, answer: str) -> rx.Component:
     return rx.box(
-        rx.box(question, text_align="right"),
-        rx.box(answer, text_align="left"),
-        margin="1em",
-        )
+        rx.box(
+            rx.text(question, style=style.question_style),
+            text_align="right",
+        ),
+        rx.box(
+            rx.text(answer, style=style.answer_style),
+            text_align="left",
+        ),
+        margin_y="1em",
+    )
+
 
 def chat() -> rx.Component:
-    qa_pairs = [
-        (
-            "¿Qué es Reflex?",
-            "Una manera de construir aplicaciones web en Python.",
-        ),
-        (
-            "¿Qué puedo hacer con Reflex?",
-            "Todo lo que puedes hacer con React, pero en Python.",
-        ),
-    ]
     return rx.box(
-        *[
-            qa(question, answer)
-            for question, answer in qa_pairs
-        ]
+        rx.foreach(
+            state.chat_history,
+            lambda messages: qa(messages[0], messages[1]),
+        )
     )
+
 
 def action_bar() -> rx.Component:
     return rx.hstack(
-        rx.input(placeholder="Haz una pregunta"),
-        rx.button("Preguntar"),
+        rx.input(
+            value=State.question,
+            placeholder="Haz una pregunta",
+            on_change=State.set_question,
+            style=style.input_style,
+        ),
+        rx.button(
+            "Enviar",
+            on_click=State.answer,
+            style=style.button_style,
+        ),
     )
+
 
 def index() -> rx.Component:
-    return rx.container(
-        chat(),
-        action_bar(),
+    return rx.center(
+        rx.vstack(
+            chat(),
+            action_bar(),
+            align="center",
+        )
     )
 
 
-# Add state and page to the app.
 app = rx.App()
 app.add_page(index)
--- Proyecto_Apollo/style.py
+++ Proyecto_Apollo/style.py
@@ -3,7 +3,7 @@ import reflex as rx
 chat_margin = "28%"
 message_style = dict(
     padding="1em",
-    border_radius="5px",
+    border_radius="16px",
     margin_y="0.5em",
     max_width="30em",
     display="inline-block",
@@ -19,10 +19,12 @@ answer_style = message_style | dict(
 )
 
 input_style = dict(
+    border_radius="8px",
     border_width="1px",
     padding="0.5em",
     width="350px",
 )
 button_style = dict(
     background_color=rx.color("accent", 10),
+        border_radius="8px",
 )
